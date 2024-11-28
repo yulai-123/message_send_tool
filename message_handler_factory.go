4ab242d7ea8ff9104c5e7150a209b3aa48847a70
thesky <thesky341@foxmail.com>
2024-11-28 11:38:45 +0800

    增加说明

--- message_handler_factory.go
+++ message_handler_factory.go
@@ -2,9 +2,14 @@ package message_send_tool
 
 import (
 	"github.com/yulai-123/message_send_tool/impl/lark"
+	"github.com/yulai-123/message_send_tool/impl/miaotixing"
 	"github.com/yulai-123/message_send_tool/model"
 )
 
 func NewLarkMessageHandler(appID, appSecret string) (model.MessageSender, error) {
 	return lark.NewMessageHandler(appID, appSecret)
+}
+
+func NewMiaotixingMessageHandler(triggerID string) (model.MessageSender, error) {
+	return miaotixing.NewMessageHandler(triggerID)
 }
